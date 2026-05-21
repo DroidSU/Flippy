8fc2c2e72a3b3e2c0655968f3ae84c8ce35cd2b7
Sujoy Datta <sujoydatta26@gmail.com>
2026-05-21 21:54:26 +0530

    Challenges added
    Signed-off-by: Sujoy Datta <sujoydatta26@gmail.com>

--- app/src/main/java/com/fliq/views/MainActivity.kt
+++ app/src/main/java/com/fliq/views/MainActivity.kt
@@ -2,6 +2,7 @@ package com.fliq.views
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,6 +11,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import com.fliq.core.settings.SettingsRepository
 import com.fliq.core.theme.FliqTheme
+import com.fliq.game_engine.models.Challenge
 import com.fliq.profile.vm.ProfileViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -38,10 +40,14 @@ class MainActivity : ComponentActivity() {
                     accuracy = accuracy,
                     unlockedBadges = unlockedBadges,
                     onChallengeSelected = { challenge ->
-                        val intent = Intent(this, GameActivity::class.java).apply {
-                            putExtra(GameActivity.EXTRA_CHALLENGE, challenge.name)
+                        if (challenge == Challenge.BLACKOUT) {
+                            Toast.makeText(this, "Coming Soon", Toast.LENGTH_SHORT).show()
+                        } else {
+                            val intent = Intent(this, GameActivity::class.java).apply {
+                                putExtra(GameActivity.EXTRA_CHALLENGE, challenge.name)
+                            }
+                            startActivity(intent)
                         }
-                        startActivity(intent)
                     },
                     onProfileClick = { startActivity(Intent(this, ProfileActivity::class.java)) },
                     onLeaderboardClick = { startActivity(Intent(this, LeaderboardActivity::class.java)) },
